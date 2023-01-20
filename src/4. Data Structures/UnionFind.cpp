b75bfd3ef4f5b3b7dd750107c480a8637a0ae193
MrYhatoh <gabriel.carmonat@sansano.usm.cl>
2023-01-19 23:11:39 -0300

    changhing tabs with only spaces

--- src/4. Data Structures/UnionFind.cpp
+++ src/4. Data Structures/UnionFind.cpp
@@ -1,22 +1,22 @@
 struct UnionFind {
-	vector<int> e;
-	UnionFind(int n) {
-		e.assign(n, -1);
-	}
-	int findSet (int x) {
-		return (e[x] < 0 ? x : e[x] = findSet(e[x]));
-	}
-	bool sameSet (int x, int y) {
-		return findSet(x) == findSet(y);
-	}
-	int size (int x) {
-		return -e[findSet(x)];
-	}
-	bool unionSet (int x, int y) {
-		x = findSet(x), y = findSet(y);
-		if (x == y) return 0;
-		if (e[x] > e[y]) swap(x, y);
-		e[x] += e[y], e[y] = x;
-		return 1;
-	}
+  vector<int> e;
+  UnionFind(int n) {
+    e.assign(n, -1);
+  }
+  int findSet (int x) {
+    return (e[x] < 0 ? x : e[x] = findSet(e[x]));
+  }
+  bool sameSet (int x, int y) {
+    return findSet(x) == findSet(y);
+  }
+  int size (int x) {
+    return -e[findSet(x)];
+  }
+  bool unionSet (int x, int y) {
+    x = findSet(x), y = findSet(y);
+    if (x == y) return 0;
+    if (e[x] > e[y]) swap(x, y);
+    e[x] += e[y], e[y] = x;
+    return 1;
+  }
 };
