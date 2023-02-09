a89d112622ee2e0ce6f9ee0d32dbb7bc2d0167d1
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-02-08 22:46:38 -0300

    Update Mathematics.cpp
    Fixed LCM

--- src/1. Initial Setup and Definitions/Mathematics.cpp
+++ src/1. Initial Setup and Definitions/Mathematics.cpp
@@ -1,4 +1,4 @@
 #define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) gcd(a, b) ? (a / gcd(a, b) * b) : 0
+#define lcm(a, b) gcd(a, b) ? ( (a*b) / gcd(a, b) : 0
 const double PI = 3.1415926535897932384626433832795;
 const ll PRIME_BASE = (1 << 61) - 1;
