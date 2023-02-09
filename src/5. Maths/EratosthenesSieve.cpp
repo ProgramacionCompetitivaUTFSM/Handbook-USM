fa339772ba24c66f76b3d8b2bb499e06b7ae8f79
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-02-08 23:03:00 -0300

    Update EratosthenesSieve.cpp

--- src/5. Maths/EratosthenesSieve.cpp
+++ src/5. Maths/EratosthenesSieve.cpp
@@ -7,10 +7,10 @@ struct EratosthenesSieve {
         isPrime.resize(n + 1, true);
         isPrime[0] = isPrime[1] = false;
 
-        for (ll i = 2; i <= n; i++) {
+        for (ll i = 2; i*i <= n; i++) {
             if (isPrime[i]) {
                 primes.push_back(i);
-                for (int j = 2 * i; j <= n; j += i)
+                for (int j = i*i; j <= n; j += i)
                     isPrime[j] = false;
             }
         }
