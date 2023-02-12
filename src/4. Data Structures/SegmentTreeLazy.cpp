47ddcbbe619cb3ea3dc707e520975daf4e560a3b
Gabriel Carmona <39179803+Yhatoh@users.noreply.github.com>
2023-02-11 22:17:36 -0300

    Update SegmentTreeLazy.cpp

--- src/4. Data Structures/SegmentTreeLazy.cpp
+++ src/4. Data Structures/SegmentTreeLazy.cpp
@@ -1,60 +1,91 @@
-template <class T>
-struct SegmentTree {
-  int N; vector<T> ST, lazy;
-  vector<bool> bit; T (*merge)(T, T);
-  void build(int n, int l, int r, vector<T> &vs) {
-    if(l == r) ST[n] = vs[l];
-    else {
-      build(2 * n, l, (r + l) / 2, vs);
-      build(2 * n + 1, (r + l) / 2 + 1, r, vs);
-      ST[n] = merge(ST[n * 2], ST[n * 2 + 1]);
+template<
+    class T1, // answer value stored on nodes
+    class T2, // lazy update value stored on nodes
+    T1 merge(T1, T1), 
+    void pushUpd(T2&, T2&, int, int, int, int), // push update value from a node to another. parent -> child
+    void applyUpd(T2&, T1&, int, int)           // apply the update value of a node to its answer value. upd -> ans
+>
+struct SegmentTreeLazy{
+    vector<T1> ST;
+    vector<T2> lazy;
+    vector<bool> upd;
+    int n;
+    void build(int i, int l, int r, vector<T1>&values){
+        if (l == r){
+            ST[i] = values[l];
+            return;
+        }
+        build(i << 1, l, (l + r) >> 1, values);
+        build(i << 1 | 1, (l + r) / 2 + 1, r, values);
+        ST[i] = merge(ST[i << 1], ST[i << 1 | 1]);
     }
-  }
-  SegmentTree(vector<T> &vs, T (*m)(T a, T b)) {
-    merge = m; N = vs.size();
-    ST.resize(4 * N + 3); lazy.assign(4 * N + 3, T());
-    bit.assign(4 * N + 3, false); build(1, 0, N - 1, vs);
-  }
-  void push(int n, int i, int j) {
-    if(bit[n]) {
-      ST[n] += lazy[n];
-      if(i != j) {
-        lazy[2 * n] += lazy[n]; 
-        lazy[2 * n + 1] += lazy[n];
-        bit[2 * n] = 1; bit[2 * n + 1] = 1;
+    SegmentTreeLazy(vector<T1>&values){
+        n = values.size();
+        ST.resize(n << 2 | 3);
+        lazy.resize(n << 2 | 3);
+        upd.resize(n << 2 | 3, false);
+        build(1, 0, n - 1, values);
+    }
+    void push(int i, int l, int r){
+      if (upd[i]){
+        applyUpd(lazy[i], ST[i], l, r);
+        if (l != r){
+          pushUpd(lazy[i], lazy[i << 1], l, r, l, (l + r) / 2);
+          pushUpd(lazy[i], lazy[i << 1 | 1], l, r, (l + r) / 2 + 1, r);
+          upd[i << 1] = 1;
+          upd[i << 1 | 1] = 1;
+        }
+        upd[i] = false;
+        lazy[i] = T2();
       }
-      lazy[n] = T(); bit[n] = 0;
-    } 
-  }
-  void apply(int n, int i, int j, T val) {
-    ST[n] += val;
-    if(i != j) {
-      lazy[2 * n] += val; 
-      lazy[2 * n + 1] += val;
-      bit[2 * n] = 1; bit[2 * n + 1] = 1;
     }
-  }
-  T query(int i, int j) {
-    return query(0, N - 1, 1, i, j); }
-  T query(int l, int r, int n, int i, int j) {
-    push(n, l, r);
-    if(i <= l && r <= j) return ST[n];
-    int mid = (r + l) / 2;
-    if(mid<i||j<l) return query(mid + 1, r, 2 * n + 1, i, j);
-    if(mid >= j || r < i) return query(l, mid, 2 * n, i, j);
-    return merge(query(l, mid, 2 * n, i, j),
-                 query(mid + 1, r, 2 * n + 1, i, j));
-  }
-  void update(int i, int j, T val) {
-    update(0, N - 1, 1, i, j, val); }
-  void update(int l,int r,int n,int i,int j, T val) {
-    push(n, l, r);
-    if(r < i || j < l) return;
-    if(i <= l && r <= j) {
-      apply(n, l, r, val); return;
+    void update(int i, int l, int r, int a, int b, T2 &u){
+        if (l >= a and r <= b){
+          pushUpd(u, lazy[i], a, b, l, r);
+          upd[i] = true;
+        }
+        push(i, l, r);
+        if (l > b or r < a) return;
+        if (l >= a and r <= b) return;
+        update(i << 1, l, (l + r) >> 1, a, b, u);
+        update(i << 1 | 1, (l + r) / 2 + 1, r, a, b, u);
+        ST[i] = merge(ST[i << 1], ST[i << 1 | 1]);
     }
-    int mid = (r + l) / 2;
-    update(l, mid, 2 * n, i, j, val);
-    update(mid + 1, r, 2 * n + 1, i, j, val);
-    ST[n] = merge(ST[2 * n], ST[2 * n + 1]);
-  }};
+    void update(int a, int b, T2 u){
+        if (a > b){
+            update(0, b, u);
+            update(a, n - 1, u);
+            return ;
+        }
+        update(1, 0, n - 1, a, b, u);
+    }
+    T1 query(int i, int l, int r, int a, int b){
+        push(i, l, r);
+        if (a <= l and r <= b)
+            return ST[i];
+        int mid = (l + r) >> 1;
+        if (mid < a)
+            return query(i << 1 | 1, mid + 1, r, a, b);
+        if (mid >= b) 
+            return query(i << 1, l, mid, a, b);
+        return merge(query(i << 1, l, mid, a, b), query(i << 1 | 1, mid + 1, r, a, b));
+    }
+    T1 query(int a, int b){
+        if (a > b){
+            return merge(query(a, n - 1), query(0, b));
+        }
+        return query(1, 0, n - 1, a, b);
+    }
+};
+
+ll merge(ll a, ll b){
+    return a + b;
+}
+
+void pushUpd(ll &u1, ll &u2, int l1, int r1, int l2, int r2){
+    u2 = u1;
+}
+
+void applyUpd(ll &u, ll &v, int l, int r){
+    v = (r - l + 1) * u;
+}
