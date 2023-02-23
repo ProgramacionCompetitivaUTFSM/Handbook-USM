c73a416e8e72009d8d81d7ec6abcfb3b9a752f40
MrYhatoh <gabriel.carmonat@sansano.usm.cl>
2023-02-23 03:34:49 -0300

    Mo's algorithm template

---
+++ src/3. Algorithms/Mo.cpp
@@ -0,0 +1,34 @@
+template< class T, class T2 > 
+// T -> elems, T2 -> Data Structure
+struct MoAlgorithm {
+	vector< T > ans;
+	MoAlgorithm(vector< T > &v, vector< Query > &queries, 
+						  void (*add)(T2&, T), void (*remove)(T2&, T), T(*answer)(T2&, Query)) {
+    // data structure needs constructor to initialize empty
+		T2 ds(v.size());
+		ans.assign(queries.size(), -1);
+		sort(queries.begin(), queries.end());
+
+		int l = 0;
+		int r = -1;
+		for(Query q : queries) {
+			while(l > q.l) {
+				l--;
+				add(ds, v[l]);
+			}
+			while (r < q.r) {
+				r++;
+				add(ds, v[r]);
+			}
+			while (l < q.l) {
+				remove(ds, v[l]);
+				l++;
+			}
+			while (r > q.r) {
+				remove(ds, v[r]);
+				r--;
+			}
+			ans[q.i] = answer(ds, q);
+		}
+	}
+};
