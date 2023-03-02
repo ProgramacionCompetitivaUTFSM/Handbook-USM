a2f8b0a54674d184691ecd4b80893c48ba2680ed
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-03-02 18:19:40 -0300

    Removing pass by reference

--- src/7. Geometry/Point2D.cpp
+++ src/7. Geometry/Point2D.cpp
@@ -1,47 +1,47 @@
-template<typename T >
+template<typename T>
 struct Point2D {
   T x, y;
   Point2D() {};
   Point2D(T x_, T y_) : x(x_), y(y_) {}
-  Point2D< T >& operator=(const Point2D< T > &t) {
+  Point2D< T >& operator=(Point2D< T > t) {
     x = t.x; y = t.y;
     return *this;
   }
-  Point2D< T >& operator+= (const Point2D< T > &t) {
+  Point2D< T >& operator+= (Point2D< T > t) {
     x += t.x; y += t.y;
     return *this;
   }
-  Point2D< T >& operator-= (const Point2D< T > &t) {
+  Point2D< T >& operator-= (Point2D< T > t) {
     x -= t.x; y -= t.y;
     return *this;
   }
-  Point2D< T >& operator*= (const Point2D< T > &t) {
-    x *= t; y *= t;
+  Point2D< T >& operator*= (Point2D< T > t) {
+    x *= t.x; y *= t.y;
     return *this;
   }
-  Point2D< T >& operator/= (const Point2D< T > &t) {
-    x /= t; y /= t;
+  Point2D< T >& operator/= (Point2D< T > t) {
+    x /= t.y; y /= t.y;
     return *this;
   }
-  Point2D< T > operator+(const Point2D< T > &t) const {
+  Point2D< T > operator+(Point2D< T > t) {
     return Point2D(*this) += t;
   }
-  Point2D< T > operator-(const Point2D< T > &t) const {
+  Point2D< T > operator-(Point2D< T > t) {
     return Point2D(*this) -= t;
   }
-  Point2D< T > operator*(T t) const {
+  Point2D< T > operator*(T t) {
     return Point2D(*this) *= t;
   }
-  Point2D< T > operator/(T t) const {
+  Point2D< T > operator/(T t) {
     return Point2D(*this) /= t;
   }
-  T dot(Point2D< T >& b) { return x * b.x + y * b.y; }
-  T cross(Point2D< T >& b) { return x * b.y - y * b.x; }
+  T dot(Point2D< T > b) { return x * b.x + y * b.y; }
+  T cross(Point2D< T > b) { return x * b.y - y * b.x; }
   T norm() { return dot(*this); }
   double abs() { return sqrt(norm()); }
-  double proj(Point2D< T >& b) { return dot(b) / b.abs(); }
-  double angle(Point2D< T >& b) { 
-    return acos(dot(b) / abs() / b.abs()); 
+  double proj(Point2D< T > b) { return dot(b) / b.abs(); }
+  double angle(Point2D< T > b) { 
+    return acos(dot(b) / this->abs() / b.abs()); 
   }
 };
 template<typename T >
