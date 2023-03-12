1127766c4596d3143ed5fb7a383efd928ad06097
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-03-12 17:36:30 -0300

    Update SegmentTree.cpp
    Ahora la función merge se pasa por template para que se pueda usar con HLD

--- src/4. Data Structures/SegmentTree.cpp
+++ src/4. Data Structures/SegmentTree.cpp
@@ -1,8 +1,7 @@
-template <class T>
+template <class T, T merge(T, T)>
 struct SegmentTree {
   int N;
   vector<T> ST;
-  T (*merge)(T, T); 
   void build(int n, int l, int r, vector<T> &vs) {
     if(l == r) ST[n] = vs[l];
     else { 
@@ -10,12 +9,16 @@ struct SegmentTree {
       build(n * 2 + 1, (r + l) / 2 + 1, r, vs);
       ST[n] = merge(ST[n * 2], ST[n * 2 + 1]);
     }
-  } 
-  SegmentTree(vector<T> &vs, T (*m)(T a, T b)) {
-    merge = m; N = vs.size();
-    ST.resize(4 * N + 3); build(1, 0, N - 1, vs);
   }
-  T query(int i, int j) { return query(0, N - 1, 1, i, j); }
+  SegmentTree() {}
+  SegmentTree(vector<T> &vs) {
+    N = vs.size();
+    ST.resize(4 * N + 3);
+    build(1, 0, N - 1, vs);
+  }
+  T query(int i, int j) {
+    return query(0, N - 1, 1, i, j);
+  }
   T query(int l, int r, int n, int i, int j) {
     if(l >= i && r <= j) return ST[n];
     int mid = (r + l) / 2;
@@ -24,7 +27,9 @@ struct SegmentTree {
     return merge(query(l, mid, n * 2, i, j),
                  query(mid + 1, r, n * 2 + 1, i, j));
   }
-  void update(int pos, T val) { update(0, N - 1, 1, pos, val); }
+  void update(int pos, T val) {
+    update(0, N - 1, 1, pos, val);
+  }
   void update(int l, int r, int n, int pos, T val) {
     if(r < pos || pos < l) return;
     if(l == r) ST[n] = val;
