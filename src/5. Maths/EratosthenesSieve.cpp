35d191c9fc1db863d08833dfe6334283cee81680
Gabriel Carmona <39179803+Yhatoh@users.noreply.github.com>
2023-05-22 15:58:43 -0400

    Update EratosthenesSieve.cpp
    int to ll

--- src/5. Maths/EratosthenesSieve.cpp
+++ src/5. Maths/EratosthenesSieve.cpp
@@ -8,7 +8,7 @@ struct EratosthenesSieve {
     for (ll i = 2; i*i <= n; i++) {
       if (isPrime[i]) {
         primes.push_back(i);
-        for (int j = i*i; j <= n; j += i)
+        for (ll j = i*i; j <= n; j += i)
           isPrime[j] = false;
       }
     }
