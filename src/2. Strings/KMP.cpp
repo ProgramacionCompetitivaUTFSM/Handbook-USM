c3998b9e78ec704f6519a58d22f2d249ce8fe245
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-06-04 01:42:12 -0400

    Update KMP.cpp
    Now tested:
    https://cses.fi/paste/48ab3c12adce9cac5e9d37/

--- src/2. Strings/KMP.cpp
+++ src/2. Strings/KMP.cpp
@@ -1,45 +1,33 @@
 struct KMP {
-  int n;
-  int m; // in case you have two strings
-  vector< int > pi;
-
-  KMP(string s) {
-    n = s.length();
-    pi.assign(n, 0);
-    for(int i = 1; i < n; i++) {
-      int j = pi[i - 1];
-      while(j > 0 && s[i] != s[j]) 
-        j = pi[j - 1];
-      if(s[i] == s[j]) j++;
-      pi[i] = j;
+  string pattern;
+  vector < int > lps;
+  KMP(string & pat): pattern(pat) {
+    lps.resize(pat.size(), 0);
+    int len = 0, i = 1;
+    while (i < pattern.size()) {
+      if (pattern[i] == pattern[len])
+        lps[i++] = ++len;
+      else {
+        if (len != 0) len = lps[len - 1];
+        else lps[i++] = 0;
+      }
     }
   }
-  KMP(string p, string t) {
-    string pt = p + "#" + t;
-    n = p.length();
-    m = t.length();
-    pi.assign(n + m + 1, 0);
-    for(int i = 1; i <  n + m + 1; i++) {
-      int j = pi[i - 1];
-      while(j > 0 && s[i] != s[j]) 
-        j = pi[j - 1];
-      if(s[i] == s[j]) j++;
-      pi[i] = j;
+  vector < int > search(const string & text) {
+    vector < int > matches;
+    int i = 0, j = 0;
+    while (i < text.size()) {
+      if (pattern[j] == text[i]) {
+        i++, j++;
+        if (j == pattern.size()) {
+          matches.push_back(i - j);
+          j = lps[j - 1];
+        }
+      } else {
+        if (j != 0) j = lps[j - 1];
+        else i++;
+      }
     }
-  }
-  void p_in_t(vector< int > &ans) {
-    for(int i = n + 1; i < n + m + 1; i++) {
-      if(pi[i] == n)
-        ans.push_back(i - 2 * n);
-    }
-  }
-  void count_occ_prefix(vector< int > &ans) {
-    ans.assign(n + 1, 0);
-    for(int i = 0; i < n; i++)
-      ans[pi[i]]++;
-    for(int i = n - 1; i > 0; i--) 
-      ans[pi[i - 1]] += ans[i];
-    for(int i = 0; i <= n; i++)
-      ans[i]++;
+    return matches;
   }
 };
