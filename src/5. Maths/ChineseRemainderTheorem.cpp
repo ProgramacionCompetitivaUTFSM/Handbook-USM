087f31c571ad416184f3639bfe6a6ed31d5f2773
MrYhatoh <gabriel.carmonat@sansano.usm.cl>
2023-06-14 01:22:58 -0400

    fix CRT

--- src/5. Maths/ChineseRemainderTheorem.cpp
+++ src/5. Maths/ChineseRemainderTheorem.cpp
@@ -1,12 +1,28 @@
-ll GCRT(vector<ll> &A, vector<ll> &N) {
-  int k = A.size();
-  ll a = A[0], n = N[0];
-  for(int i = 1; i < k; ++i) {
-    vector<ll> v = egcd(n,N[i]); 
-    ll g = v[0], m1 = v[1], m2 = v[2];
-    if((a - A[i])%g != 0) return -1;
-    ll nn = N[i]/g*n; a = (a*m2%nn*(N[i]/g) + A[i]*m1%nn*(n/g))%nn;
-    n = nn; if(a < 0) a += n;
+struct GCD_type { ll x, y, d; };
+GCD_type ex_GCD(ll a, ll b){
+  if (b == 0) return {1, 0, a};
+  GCD_type pom = ex_GCD(b, a % b);
+  return {pom.y, pom.x - a / b * pom.y, pom.d};
+}
+ll crt(vector<ll> a, vector<ll> m){
+  int n = a.size();
+  for (int i = 0; i < n; i ++){
+    a[i] %= m[i];
+    a[i] = a[i] < 0 ? a[i] + m[i] : a[i];
   }
-  return a;
+  ll ans = a[0];
+  ll M = m[0];
+  for (int i = 1; i < n; i ++){
+    auto pom = ex_GCD(M, m[i]);
+    ll x1 = pom.x;
+    ll d = pom.d;
+    if ((a[i] - ans) % d != 0)
+      return -1;
+    ans = ans + x1 * (a[i] - ans) / d % (m[i] / d) * M;
+    M = M * m[i] / d;
+    ans %= M;
+    ans = ans < 0 ? ans + M : ans;
+    M = M / __gcd(M, m[i]) * m[i];
+  }
+  return ans;
 }
