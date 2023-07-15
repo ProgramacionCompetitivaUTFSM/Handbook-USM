f4f2686fb46218c11ea72a4e4c7bf19105213eaf
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-07-14 23:22:10 -0400

    Create PrefixTree.cpp
    Aporte de: CharlesLakes

---
+++ src/2. Strings/PrefixTree.cpp
@@ -0,0 +1,18 @@
+struct PrefixTree {
+  vector <vector <ll>> tree;
+  PrefixTree() {
+    tree.push_back(vector < ll > (26, -1));
+  };
+  void insert(string & s, ll i = 0, ll u = 0) {
+    if (s.size() == i) return;
+    char c = s[i];
+    if (tree[u][c - 'a'] != -1)
+      insert(s, i + 1, tree[u][c - 'a']);
+    else {
+      ll pos = tree.size();
+      tree.push_back(vector < ll > (26, -1));
+      tree[u][c - 'a'] = pos;
+      insert(s, i + 1, tree[u][c - 'a']);
+    }
+  }
+};
