dd1e924d15c33630399f32ee36409936621409d1
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-08-20 22:25:54 -0400

    Create FastRollingHashing.cpp

---
+++ src/2. Strings/FastRollingHashing.cpp
@@ -0,0 +1,22 @@
+template<class T>
+struct RollingHashing {
+    int base, mod; 
+    vector<int> p, H;
+    int n;
+
+    RollingHashing(const T &s, int b, int m): base(b), mod(m), n(s.size()) {
+        p.assign(n+1, 1);
+        H.assign(n+1, 0);
+
+        for (int i = 0; i < n; ++i) {
+            H[i+1] = (H[i] * base + s[i]) % mod;
+            p[i+1] = (p[i] * base) % mod;
+        }
+    }
+
+    int get(int l, int r) {
+        int res = (H[r+1] - H[l]*p[r-l+1]) % mod;
+        if (res < 0) res += mod;
+        return res;
+    }
+};
