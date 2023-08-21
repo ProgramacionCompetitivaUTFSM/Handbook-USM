11d5232a146936105fbe0dc370c3efdb190b55e6
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-08-21 00:23:11 -0400

    Update Z.cpp
    Fixed Z algorithm

--- src/2. Strings/Z.cpp
+++ src/2. Strings/Z.cpp
@@ -1,38 +1,38 @@
 struct Z {
-  int n;
-  int m; // in case you have two strings
-  vector< int > z;
-  Z(string s) {
-    n = s.lentgh();
-    z.assign(n, 0);
-    for(int i = 1; i < n; i++) {
-      if(i <= r)
-        z[i] = min(r - i + 1, z[i - 1]);
-      while(i + z[i] < n && s[z[i]] == s[i + z[i]])
-        ++z[i];
-      if(i + z[i] - 1 > r)
-        l = i, r = i + z[i] - 1;
+    int n, m; 
+    vector<int> z;
+    Z(string s) {
+        n = s.size();
+        z.assign(n, 0);
+        int l = 0, r = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= r)
+                z[i] = min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+                ++z[i];
+            if (i + z[i] - 1 > r)
+                l = i, r = i + z[i] - 1;
+        }
     }
-  }
-  Z(string p, string t) {
-    // if # exists use other char
-    string s = p + "#" + t;
-    n = p.size();
-    m = p.size();
-    z.assing(n + m + 1, 0);
-    for(int i = 1; i < n; i++) {
-      if(i <= r)
-        z[i] = min(r - i + 1, z[i - 1]);
-      while(i + z[i] < n && s[z[i]] == s[i + z[i]])
-        ++z[i];
-      if(i + z[i] - 1 > r)
-        l = i, r = i + z[i] - 1;
+    Z(string p, string t) {
+        string s = p + "#" + t;
+        n = p.size();
+        m = t.size();
+        z.assign(n + m + 1, 0);
+        int l = 0, r = 0;
+        for (int i = 1; i < n + m + 1; i++) {
+            if (i <= r)
+                z[i] = min(r - i + 1, z[i - l]);
+            while (i + z[i] < n + m + 1 && s[z[i]] == s[i + z[i]])
+                ++z[i];
+            if (i + z[i] - 1 > r)
+                l = i, r = i + z[i] - 1;
+        }
     }
-  }
-  void p_in_t(vector< int > &ans) {
-    for(int i = 0; i < m; i++) {
-      if(z[i + n + 1] == n)
-        ans.push_back(i);
+    void p_in_t(vector<int>& ans) {
+        for (int i = n + 1; i < n + m + 1; i++) {
+            if (z[i] == n)
+                ans.push_back(i - n - 1);
+        }
     }
-  }
 };
