cbd8a212cca21589af24c2e902777e71c88b32d1
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-08-31 21:03:35 -0400

    Update DigitDP.cpp

--- src/8. DP/DigitDP.cpp
+++ src/8. DP/DigitDP.cpp
@@ -1,31 +1,22 @@
 int dp[12][12][2]; // dp[i][s][f] {i: posicion, s: estado del problema, f: act < s}
 int k, d;
-// solve(r) - solve(l-1)
-int call(int pos, int cnt, int f){
-    if(cnt > k) return 0;
-    if(pos == num.size()){
-        if(cnt == k) return 1;
-        return 0;
-    }
-    if(dp[pos][cnt][f] != -1) return dp[pos][cnt][f];
-    int res = 0, LMT;
-    if(f == 0) LMT = num[pos];
-    else LMT = 9;
-    /// Try to place all the valid digits such that the number doesn't exceed b
-    for(int dgt = 0; dgt<=LMT; dgt++){
-        int nf = f, ncnt = cnt;
-        if(f == 0 && dgt < LMT) nf = 1; /// The number is getting smaller at this position
-        if(dgt == d) ncnt++;
-        if(ncnt <= k) res += call(pos+1, ncnt, nf);
+
+int call(int pos, int cnt, int f) {
+    if (cnt > k) return 0;
+    if (pos == num.size()) return (cnt == k) ? 1 : 0;
+    if (dp[pos][cnt][f] != -1) return dp[pos][cnt][f];
+    int res = 0, LMT = (f == 0) ? num[pos] : 9;
+    for (int dgt = 0; dgt <= LMT; dgt++) {
+        int nf = f, ncnt = cnt + (dgt == d);
+        if (f == 0 && dgt < LMT) nf = 1;
+        res += call(pos + 1, ncnt, nf);
     }
     return dp[pos][cnt][f] = res;
 }
-int solve(string s){
+
+int solve(string s) {
     num.clear();
-    while(s.size()){
-        num.push_back((s.back()-'0')%10);
-        s.pop_back();
-    }
+    for (char c : s) num.push_back((c - '0') % 10);
     reverse(num.begin(), num.end());
     memset(dp, -1, sizeof(dp));
     return call(0, 0, 0);
