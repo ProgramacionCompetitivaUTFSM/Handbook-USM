82c28ee1a003e161a2620f5e5d22a36306ff60e3
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-08-31 21:46:50 -0400

    Update Divisors.cpp

--- src/5. Maths/Divisors.cpp
+++ src/5. Maths/Divisors.cpp
@@ -1,20 +1,19 @@
-// Obtener todos los divisores de un número en O(sqrt(n))
 void getDivisors(int n, vector<int> &ans) {
-    vector<int> left, right;
-    for (int i = 1; i*i <= n; i++)
-        if (n % i == 0) {
-            if (i != n/i)
-                right.push_back(n/i);
-            left.push_back(i);
-        }
-    ans.resize(left.size() + right.size());
-    reverse(all(right));
-    int i = 0, j = 0;
-    while (i < left.size() and j < right.size()) {
-        if (left[i] < right[j]) 
-            ans[i+j-1] = left[i++];
-        else ans[i+j-1] = right[j++];
+  vector<int> left, right;
+  for (int i = 1; i * i <= n; i++)
+    if (n % i == 0) {
+      if (i != n / i)
+        right.push_back(n / i);
+      left.push_back(i);
     }
-    while(i < left.size()) ans[i+j-1] = left[i++];    
-    while(j < right.size()) ans[i+j-1] = right[j++];
+  ans.resize(left.size() + right.size());
+  reverse(all(right));
+  int i = 0, j = 0;
+  while (i < left.size() and j < right.size()) {
+    if (left[i] < right[j]) 
+      ans[i + j - 1] = left[i++];
+    else ans[i + j - 1] = right[j++];
+  }
+  while(i < left.size()) ans[i + j - 1] = left[i++];    
+  while(j < right.size()) ans[i + j - 1] = right[j++];
 }
