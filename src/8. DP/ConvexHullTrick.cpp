0c5577974aa89740428375eafc87ee53433f96de
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-08-31 21:00:57 -0400

    Update ConvexHullTrick.cpp

--- src/8. DP/ConvexHullTrick.cpp
+++ src/8. DP/ConvexHullTrick.cpp
@@ -1,23 +1,16 @@
-#include "../Header.cpp"
-
 struct Line {
     ll m, c, id;
-    ll calc(ll x) {
-	
-        return m * x + c;
-    }
+    ll calc(ll x) { return m * x + c;}
 };
-bool obsolete(Line a, Line b, Line c){
-    return (c.c - a.c) * (a.m - b.m) < (a.m - c.m) * (b.c - a.c); 
+bool obsolete(Line a, Line b, Line c) {
+    return (c.c - a.c) * (a.m - b.m) < (a.m - c.m) * (b.c - a.c);
 }
-
-vector<Line>lines;
+vector<Line> lines;
 void insert(Line l) {
-    while(lines.size() > 1) {
+    while (lines.size() > 1) {
         ll sz = lines.size();
-        if(obsolete(lines[sz-2], lines[sz-1], l)){
-            lines.pop_back();
-        } else break;
+        if (obsolete(lines[sz - 2], lines[sz - 1], l)) lines.pop_back();
+        else { break; }
     }
     lines.push_back(l);
 }
