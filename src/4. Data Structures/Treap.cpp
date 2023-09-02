ebc83e10bbae7ac8ac4742044c8d545db98a03d2
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-09-01 22:54:10 -0400

    Update Treap.cpp

--- src/4. Data Structures/Treap.cpp
+++ src/4. Data Structures/Treap.cpp
@@ -1,21 +1,16 @@
-// https://uchile.progcomp.cl/apunte/extras/treaps/
 struct treap {
   static mt19937_64 MT;
   struct node {
-    node *left, *right;
-    ll key, priority;
-    ll value, max_value;
+    node *left, *right; ll key, priority, value, max_value;
     node(ll k, ll v = 0) {
-      left = right = NULL; key = k; priority = MT();
+      left = right = NULL; key = k; priority = MT(); 
       max_value = value = v;
-    }    
+    }
   };
   ll value(node* T) { return T ? T->value : -INF; }
   ll max_value(node* T) { return T ? T->max_value : -INF; }
-  void update(node* T) {
-    T->max_value = 
-    max({T->value, max_value(T->left), max_value(T->right)});
-  }
+  void update(node* T) { 
+    T->max_value = max({T->value, max_value(T->left), max_value(T->right)});}
   node *root;
   void merge(node* &T, node* T1, node* T2) {
     if(T1 == NULL) { T = T2; return; }
@@ -30,28 +25,22 @@ struct treap {
   }
   void split(node* T, ll x, node* &T1, node* &T2) {
     if(T == NULL) { T1 = T2 = NULL; return; }
-    if(T->key <= x) {
-      split(T->right, x, T->right, T2); T1 = T;
-    } else {
-      split(T->left, x, T1, T->left); T2 = T;
-    }
+    if(T->key <= x) { split(T->right, x, T->right, T2); T1 = T; } 
+    else { split(T->left, x, T1, T->left); T2 = T; }
     return update(T);
   }
-  void split(node* T, ll x, ll y,
-            node* &T1, node* &T2, node* &T3) {
+  void split(node* T, ll x, ll y, node* &T1, node* &T2, node* &T3) {
     split(T, x-1, T1, T2);  split(T2, y, T2, T3);
   }
   bool search(node* T, ll x) {
-    if(T == NULL) return false;
-    if(T->key == x) return true;
+    if(T == NULL) return false; if(T->key == x) return true;
     if(x < T->key) return search(T->left, x);
     return search(T->right, x);
   }
   void insert(node* &T, node* n) {
     if(T == NULL) { T = n; return; }
     if(n->priority > T->priority) {
-      split(T, n->key, n->left, n->right);
-      T = n;
+      split(T, n->key, n->left, n->right); T = n;
     } else if(n->key < T->key) insert(T->left, n);
     else insert(T->right, n);
     return update(T);
@@ -77,40 +66,22 @@ struct treap {
     if(k < T->key) return find(T->left, k);
     return find(T->right, k);
   }
-  treap() { root = NULL; }
-  treap(ll x) { root = new node(x); }
-  treap &merge(treap &O) {
-    merge(root, root, O.root);
-    return *this;
-  }
-  treap split(ll x) {
-    treap ans;
-    split(root, x, root, ans.root);
-    return ans;
-  }
-  bool search(ll x) { return search(root, x); }
-  void insert(ll x) {
-    if(search(root, x)) return;
-    return insert(root, new node(x));
-  }
-  void erase(ll x) { return erase(root, x); }
-  void set(ll k, ll v) {
-    if(set(root, k, v)) return;
-    insert(root, new node(k, v));
-  }
+  treap() {root = NULL;}
+  treap(ll x) {root = new node(x);}
+  treap &merge(treap &O) {merge(root, root, O.root); return *this; }
+  treap split(ll x) {treap ans; split(root, x, root, ans.root); return ans; }
+  bool search(ll x) {return search(root, x); }
+  void insert(ll x) {if(search(root, x)) return; return insert(root, new node(x));}
+  void erase(ll x) {return erase(root, x); }
+  void set(ll k, ll v) {if(set(root, k, v)) return; insert(root, new node(k, v));}
   ll operator[](ll k) {
     node* n = find(root, k);
-    if(n == NULL)
-      n = new node(k), insert(root, n);
-    return n->value;
+    if(n == NULL) n = new node(k), insert(root, n); return n->value;
   }
   ll query(ll a, ll b) {
-    node *T1, *T2, *T3;
-    split(root, a, b, T1, T2, T3);
-    ll ans = max_value(T2);
-    merge(root, T1, T2, T3);
+    node *T1, *T2, *T3; split(root, a, b, T1, T2, T3);
+    ll ans = max_value(T2); merge(root, T1, T2, T3);
     return ans;
   }
 };
-mt19937_64 treap::MT(chrono::system_clock::now().
-                    time_since_epoch().count());
+mt19937_64 treap::MT(chrono::system_clock::now().time_since_epoch().count());
