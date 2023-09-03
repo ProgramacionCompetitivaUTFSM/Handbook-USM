5956a58a650a23bb684346088ca6055bb2eac50a
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-09-03 19:05:01 -0300

    Update ConvexHull.cpp
    Arreglando Convex Hull. Ahora sí que sí, está testeado!

--- src/7. Geometry/ConvexHull.cpp
+++ src/7. Geometry/ConvexHull.cpp
@@ -1,25 +1,30 @@
-// Monotone chain Convex Hull
-template <typename T>
-vector<Point2D<T>> convexHull(vector<Point2D<T>> &P, bool collinear = 1) {
-  int N = P.size(), k = 0;
-  sort(P.begin(), P.end(), [](const Point2D<T> &p1, const Point2D<T> &p2) {
-    if (p1.x == p2.x) return p1.y < p2.y;
-    return p1.x < p2.x;
+template<typename T>
+vector<Point2D<T>> convexHull(vector<Point2D<T>> cloud, bool ac = 0) {
+  int n = cloud.size(), k = 0;
+  sort(cloud.begin(), cloud.end(), [](Point2D<T> &a, Point2D<T> &b) {
+    return a.x < b.x or (a.x == b.x and a.y < b.y);
   });
-  vector<Point2D<int>> H(2*N);
-  // Build lower hull
-  for(int i = 0; i < N; ++i) {
-    while(collinear and k >= 2 and H[k-2].cross(H[k-1], P[i]) < 0) k--;
-    while(!collinear and k >= 2 and H[k-2].cross(H[k-1], P[i]) <= 0) k--;
-    H[k++] = P[i];
+  if (n <= 2 or (ac and n <= 3)) return cloud;
+  bool allCollinear = true;
+  for (int i = 2; i < n; ++i) {
+    if (((cloud[1] - cloud[0]) ^ (cloud[i] - cloud[0])) != 0) {
+      allCollinear = false; break;
+    }
   }
-  // Build upper hull
-  for(int i = N-1, t = k+1; i > 0; --i) {
-    while(collinear and k >= t and H[k-2].cross(H[k-1], P[i-1]) < 0) k--;
-    while(!collinear and k >= t and H[k-2].cross(H[k-1], P[i-1]) <= 0) k--;
-    H[k++] = P[i-1];
-  }
-  // k para agregar primer punto al final
-  H.resize(k-1);
-  return H;
+  if (allCollinear) return cloud;
+  vector<Point2D<T>> ch(2 * n);
+  auto process = [&](int st, int end, int stp, int t, auto cmp) {
+    for (int i = st; i != end; i += stp) {
+      while (k >= t and cmp(ch[k - 1], ch[k - 2], cloud[i])) k--;
+      ch[k++] = cloud[i];
+    }
+  };
+  process(0, n, 1, 2, [&](auto a, auto b, auto c) {
+    return ((a - b) ^ (c - b)) < 0;
+  });
+  process(n - 2, -1, -1, k + 1, [&](auto a, auto b, auto c) {
+    return ((a - b) ^ (c - b)) < (ac ? 0 : 1);
+  });
+  ch.resize(k - 1);
+  return ch;
 }
