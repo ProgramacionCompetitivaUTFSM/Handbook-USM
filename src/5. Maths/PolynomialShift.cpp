945a1b5fd1eca144f651a664013c390cd63b8a1f
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-09-05 01:10:27 -0300

    Create PolynomialShift.cpp
    Tested: https://codeforces.com/gym/104555/problem/K

---
+++ src/5. Maths/PolynomialShift.cpp
@@ -0,0 +1,19 @@
+// solves f(x + c) = \sum_0^n-1 b_i*x^i
+vector<int> polyShift(vector<int> &a, int shift) {
+  // change for any mod for ntt
+  const int mod = 998244353;
+  NTT<998244353, 3> ntt;
+  int n = a.size() - 1;
+  Factorial f(n, mod);
+  vector<int> x(n+1), y(n+1);
+  int cur = 1;
+  for (int i = 0; i <= n; i++) {
+    x[i] = cur * f.finv[i] % mod;
+    cur = (cur * shift) % mod;
+    y[i] = a[n - i] * f.f[n-i] % mod;
+  }
+  vector<int> tmp = ntt.conv(x, y), res(n+1);
+  for (int i = 0; i <= n; i++)
+    res[i] = tmp[n-i] * f.finv[i] % mod;
+  return res;
+}
