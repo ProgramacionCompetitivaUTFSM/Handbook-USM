68cb08f1308186e748d8599a85f06eb5fb564e8d
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2023-09-08 17:16:17 -0300

    Update BellmanFord.cpp

--- src/6. Graphs/BellmanFord.cpp
+++ src/6. Graphs/BellmanFord.cpp
@@ -1,16 +1,28 @@
-void BellmanFord(int s) {
-  // remember to assign INF in vector D
-  D[s] = 0;
-  bool flag = false;
-  for(int i = 0; i < n; i++) {
-    for(int a = 0; a < n; a++) {
-      for(pi e : G[a]) {
-        b = e.first;
-        w = e.second;
-        // this is to check negative cycle
-        if(i == n - 1) flag = (D[b] > D[a] + w ? true : false); 
-        else D[b] = min(D[b], D[a] + w);
-      }
+struct Edge { int from, to, weight; };
+struct BellmanFord {
+  int n, last_updated = -1; const int INF = 1e18;
+  vector<int> p, dist;
+  BellmanFord(vector<Edge> &G, int s) {
+    n = G.size(); dist.assign(n+2, INF);
+    p.assign(n+2, -1); dist[s] = 0;
+    for (int i = 1; i <= n; i++) {
+      last_updated = -1;
+      for (Edge &e : G)
+        if (dist[e.from] + e.weight < dist[e.to]) {
+          dist[e.to] = dist[e.from] + e.weight;
+          p[e.to] = e.from; last_updated = e.to;
+        }
     }
   }
-}
+  bool getCycle(vector<int> &cycle) {
+    if (last_updated == -1) return false;
+    for (int i = 0; i < n-1; i++)
+      last_updated = p[last_updated];
+    for (int x = last_updated ;; x=p[x]) {
+      cycle.push_back(x);
+      if (x == last_updated and cycle.size() > 1) break;
+    }
+    reverse(cycle.begin(), cycle.end());
+    return true;
+  }
+};
