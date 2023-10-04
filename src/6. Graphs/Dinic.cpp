328f4e7dbdc93742f79bde228d3323323e4c8653
Gabriel Carmona <39179803+Yhatoh@users.noreply.github.com>
2023-10-04 03:39:48 -0300

    Update Dinic.cpp

--- src/6. Graphs/Dinic.cpp
+++ src/6. Graphs/Dinic.cpp
@@ -1,37 +1,37 @@
 //https://github.com/PabloMessina/Competitive-Programming-Material/blob/master/Graphs/Dinic.cpp
 struct Dinic {
-  struct Edge { int to, rev; int f, c; };
-  int n, t_; vector<vector<Edge>> G;
-  vector<int> D;
-  vector<int> q, W;
-  bool bfs(int s, int t) {
+  struct Edge { ll to, rev; ll f, c; };
+  ll n, t_; vector<vector<Edge>> G;
+  vector<ll> D;
+  vector<ll> q, W;
+  bool bfs(ll s, ll t) {
     W.assign(n, 0); D.assign(n, -1); D[s] = 0;
-    int f = 0, l = 0; q[l++] = s;
+    ll f = 0, l = 0; q[l++] = s;
     while (f < l) {
-      int u = q[f++];
+      ll u = q[f++];
       for (const Edge &e : G[u]) if (D[e.to] == -1 && e.f < e.c)
         D[e.to] = D[u] + 1, q[l++] = e.to;
     }
     return D[t] != -1;
   }
-  int dfs(int u, int f) {
+  ll dfs(ll u, ll f) {
     if (u == t_) return f;
-    for (int &i = W[u]; i < (int)G[u].size(); ++i) {
-      Edge &e = G[u][i]; int v = e.to;
+    for (ll &i = W[u]; i < (ll)G[u].size(); ++i) {
+      Edge &e = G[u][i]; ll v = e.to;
       if (e.c <= e.f || D[v] != D[u] + 1) continue;
-      int df = dfs(v, min(f, e.c - e.f));
+      ll df = dfs(v, min(f, e.c - e.f));
       if (df > 0) { e.f += df, G[v][e.rev].f -= df; return df; }
     }
     return 0;
   }
-  Dinic(int N) : n(N), G(N), D(N), q(N) {}
-  void add_edge(int u, int v, int cap) {
-    G[u].push_back({v, (int)G[v].size(), 0, cap});
-    G[v].push_back({u, (int)G[u].size() - 1, 0, 0}); // Use cap instead of 0 if bidirectional
+  Dinic(ll N) : n(N), G(N), D(N), q(N) {}
+  void add_edge(ll u, ll v, ll cap) {
+    G[u].push_back({v, (ll)G[v].size(), 0, cap});
+    G[v].push_back({u, (ll)G[u].size() - 1, 0, 0}); // Use cap instead of 0 if bidirectional
   }
-  int max_flow(int s, int t) {
-    t_ = t; int ans = 0;
-    while (bfs(s, t)) while (int dl = dfs(s, LLONG_MAX)) ans += dl;
+  ll max_flow(ll s, ll t) {
+    t_ = t; ll ans = 0;
+    while (bfs(s, t)) while (ll dl = dfs(s, LLONG_MAX)) ans += dl;
     return ans;
   }
 };
