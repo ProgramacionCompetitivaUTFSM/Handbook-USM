c46a4f65a7548751f5472d9b75e9cca595f03b9f
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-01-05 13:44:58 -0300

    Update Divisors.cpp
    Changed int to ll

--- src/5. Maths/Divisors.cpp
+++ src/5. Maths/Divisors.cpp
@@ -1,14 +1,14 @@
-void getDivisors(int n, vector<int> &ans) {
-  vector<int> left, right;
-  for (int i = 1; i * i <= n; i++)
+void getDivisors(ll n, vector<ll> &ans) {
+  vector<ll> left, right;
+  for (ll i = 1; i * i <= n; i++)
     if (n % i == 0) {
       if (i != n / i)
         right.push_back(n / i);
       left.push_back(i);
     }
   ans.resize(left.size() + right.size());
-  reverse(all(right));
-  int i = 0, j = 0;
+  reverse(begin(right), end(right));
+  ll i = 0, j = 0;
   while (i < left.size() and j < right.size()) {
     if (left[i] < right[j]) 
       ans[i + j - 1] = left[i++];
