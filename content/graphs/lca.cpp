14d98b64ae2ab1570b4146de67c246954480bbc4
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-02-09 22:09:06 -0300

    Update lca.cpp

--- content/graphs/lca.cpp
+++ content/graphs/lca.cpp
@@ -3,18 +3,18 @@
 */
 struct LCA {
   vector<vector<int>> T, parent;
-  vector<int> depth;
+  vector<int> depth, vis;
   int LOGN, V;
-  // Si da WA, probablemente el logn es muy chico
-  LCA(vector<vector<int>> &T, int logn = 20) {
-    this->LOGN = logn;
-    this->T = T;
-    T.assign(T.size()+1, vector<int>());
+  // If WA, bigger logn?
+  LCA(vector<vector<int>> &T, int logn = 20): T(T), LOGN(logn), vis(T.size()) {
     parent.assign(T.size()+1, vector<int>(LOGN, 0));
     depth.assign(T.size()+1, 0);
-    dfs();
+    // If is forest
+    //for (int u = 0; u < T.size(); u++)
+    //  if (!vis[u]) dfs(u);
   }
   void dfs(int u = 0, int p = -1) {
+    vis[u] = true;
     for (int v : T[u]) {
       if (p != v) {
         depth[v] = depth[u] + 1;
