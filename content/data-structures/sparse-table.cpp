ac771ebaef1801b159ff16af9028d2ed1e492b25
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-02-10 01:52:15 -0300

    Update sparse-table.cpp

--- content/data-structures/sparse-table.cpp
+++ content/data-structures/sparse-table.cpp
@@ -4,12 +4,12 @@
  - Does not allow updates
  *Status:* Highly tested
 */
-template <typename T>
+template <typename T, T m_(T, T)>
 struct sparse_table {
   int n;
   vector<vector<T>> table;
-  function<T(T, T)> merge;
-  sparse_table(const vector<T> &arr, function<T(T, T)> m) : merge(m) {
+  sparse_table() {}
+  sparse_table(vector<T> &arr) {
     n = arr.size();
     int k = log2_floor(n) + 1;
     table.assign(n, vector<T>(k));
@@ -17,11 +17,11 @@ struct sparse_table {
       table[i][0] = arr[i];
     for (int j = 1; j < k; j++)
       for (int i = 0; i + (1 << j) <= n; i++)
-        table[i][j] = merge(table[i][j-1], table[i+(1<<(j-1))][j-1]);
+        table[i][j] = m_(table[i][j-1], table[i+(1<<(j-1))][j-1]);
   }
   T query(int l, int r) {
     int k = log2_floor(r - l + 1);
-    return merge(table[l][k], table[r-(1 << k)+1][k]);
+    return m_(table[l][k], table[r-(1 << k)+1][k]);
   }
   int log2_floor(int n) { return n ? __builtin_clzll(1) - __builtin_clzll(n) : -1; }
 };
