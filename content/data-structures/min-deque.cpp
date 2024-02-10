b71f6f119633c6e299baa8fae8880588ec3568f8
Sebastian Torrealba <sebastian.torrealba@usm.cl>
2024-02-10 03:10:04 -0300

    Changed tab for spaces

--- content/data-structures/min-deque.cpp
+++ content/data-structures/min-deque.cpp
@@ -4,28 +4,28 @@
 */
 template<typename T>
 struct min_deque {
-min_stack<T> l, r, t;
-	void rebalance() {
-		bool f = false;
-		if (r.empty()) {f = true; l.swap(r);}
-		int sz = r.size() / 2;
-		while (sz--) {t.push(r.top()); r.pop();}
-		while (!r.empty()) {l.push(r.top()); r.pop();}
-		while (!t.empty()) {r.push(t.top()); t.pop();}
-		if (f) l.swap(r);
-	}
-	int getmin() {
-		if (l.empty()) return r.getmin();
-		if (r.empty()) return l.getmin();
-		return min(l.getmin(), r.getmin());
-	}
+  min_stack<T> l, r, t;
+  void rebalance() {
+    bool f = false;
+    if (r.empty()) {f = true; l.swap(r);}
+    int sz = r.size() / 2;
+    while (sz--) {t.push(r.top()); r.pop();}
+    while (!r.empty()) {l.push(r.top()); r.pop();}
+    while (!t.empty()) {r.push(t.top()); t.pop();}
+    if (f) l.swap(r);
+  }
+  int getmin() {
+    if (l.empty()) return r.getmin();
+    if (r.empty()) return l.getmin();
+    return min(l.getmin(), r.getmin());
+  }
   bool empty() {return l.empty() and r.empty();}
-	int size() {return l.size() + r.size();}
-	void push_front(int x) {l.push(x);}
-	void push_back(int x) {r.push(x);}
-	void pop_front() {if (l.empty()) rebalance(); l.pop();}
-	void pop_back() {if (r.empty()) rebalance(); r.pop();}
-	int front() {if (l.empty()) rebalance(); return l.top();}
-	int back() {if (r.empty()) rebalance(); return r.top();}
-	void swap(min_deque &x) {l.swap(x.l); r.swap(x.r);}
+  int size() {return l.size() + r.size();}
+  void push_front(int x) {l.push(x);}
+  void push_back(int x) {r.push(x);}
+  void pop_front() {if (l.empty()) rebalance(); l.pop();}
+  void pop_back() {if (r.empty()) rebalance(); r.pop();}
+  int front() {if (l.empty()) rebalance(); return l.top();}
+  int back() {if (r.empty()) rebalance(); return r.top();}
+  void swap(min_deque &x) {l.swap(x.l); r.swap(x.r);}
 };
