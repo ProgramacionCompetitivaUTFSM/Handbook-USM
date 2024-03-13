8764eb6ca05d116c785288747928f4a1886932b7
GC <39179803+Yhatoh@users.noreply.github.com>
2024-03-13 11:58:34 +0100

    Update centroid-decomposition.cpp

--- content/graphs/centroid-decomposition.cpp
+++ content/graphs/centroid-decomposition.cpp
@@ -1,33 +1,55 @@
 /*
  *Description:* centroid decomposition algorithm
- *Status:* Partially Tested
+ *Status:* No Tested
 */
 struct centroid_decomp {
-  int n, centroid;
-  vector<bool> vis;
-  vector<int> subtr;
-  void dfs(int v, vector< vector< int > > & adj){
-    vis[v] = 1;
-    subtr[v]++;
-    for (int to : adj[v])
-      if (!vis[to]){
-        dfs(to,adj);
-        subtr[v] += subtr[to];
-      }
-  }
-  int get_centroid(int v, int p, vector< vector< int > > &adj){
-    for (int to : adj[v]) {
-      if (to == p) continue;
-      if (subtr[to] > n/2) return get_centroid(to,v,adj);
+    int n;
+    vector<bool> vis;
+    vector<int> subtr, parent;
+    vector<vector<int>> adj;
+    // Opcional
+    // ----------
+    void dfs(int v,int p = -1){
+        for(int u: adj[v]){
+            if(u != p && !vis[u]) dfs(u,v);
+        }
     }
-    return v;
-  }
-  centroid_decomp(vector< vector< int > > &adj){
-    n = adj.size();
-    subtr.resize(n,0);
-    parent.resize(n,-1);
-    vis.resize(n,0);
-    dfs(0,adj);
-    centroid = get_centroid(0,0,adj);
-  }
+    // ----------
+
+    int calculate_subtree(int v, int p){
+        subtr[v] = 1;
+        for (int to : adj[v]){
+            if (to == p || vis[to]) continue;
+            subtr[v] += calculate_subtree(to,v);
+        }
+        return subtr[v];
+    }
+    int get_centroid(int v, int p, int sz){
+        for (int to : adj[v]){
+            if (to == p || vis[to]) continue;
+            if (subtr[to]*2 > sz) return get_centroid(to,v,sz);
+        }
+        return v;
+    }
+    int build(int v = 0){
+        int centroid = get_centroid(v,v,calculate_subtree(v,v));
+        // Opcional
+        // ----------
+        dfs(centroid);
+        // ----------
+        vis[centroid] = 1;
+        for (int to : adj[centroid]){
+            if (vis[to]) continue;
+            parent[build(to)] = centroid;
+        }
+        return centroid;
+    }
+    centroid_decomp(vector<vector<int>> &_adj){
+        adj = _adj;
+        n = adj.size();
+        subtr.resize(n,0);
+        parent.resize(n,-1);
+        vis.resize(n,0);
+        build();
+    }
 };
