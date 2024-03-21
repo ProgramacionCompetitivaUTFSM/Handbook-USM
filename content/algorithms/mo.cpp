323a023c66b89221850affc7c3316d3f985d8ad2
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-03-21 00:13:50 -0300

    Update mo.cpp
    Changed!
    tkm gabo!

--- content/algorithms/mo.cpp
+++ content/algorithms/mo.cpp
@@ -2,23 +2,29 @@
  *Description:* Allow us to answer queries offline with sqrt decomposition
  *Status:* Tested
 */
-template<class T, class T2>
-struct MoAlgorithm {
+struct query {
+  ll l, r, i;
+};
+template<class T, class T2, void add(T2&, T), void rem(T2&, T), T answer(T2&, query)>
+struct mo_algorithm {
   vector<T> ans;
-// data structure needs constructor to initialize empty
-  MoAlgorithm(vector<T> &v, vector<Query> &queries,
-              void (*add)(T2 &, T), void (*remove)(T2 &, T), T (*answer)(T2 &, Query)) {
+  mo_algorithm(vector<T> &v, vector<query> &queries) {
+    ll block_sz = 1;
     T2 ds(v.size());
     ans.assign(queries.size(), -1);
-    sort(queries.begin(), queries.end());
-    int l = 0;
-    int r = -1;
-
-    for (Query q : queries) {
-      while (l > q.l) { l--; add(ds, v[l]); }
-      while (r < q.r) { r++; add(ds, v[r]); }
-      while (l < q.l) { remove(ds, v[l]); l++; }
-      while (r > q.r) { remove(ds, v[r]); r--; }
+    block_sz = sqrtl(v.size());
+    sort(queries.begin(), queries.end(), [&](auto &a, auto &b) {
+      int ba = a.l/block_sz;
+      int bb = b.l/block_sz;
+      if(ba != bb) return ba < bb;
+      return (ba&1) ? (a.r<b.r) : (a.r>b.r);
+    }); 
+    int l = 0, r = -1;
+    for (query q : queries) {
+      while (l > q.l) l--, add(ds, v[l]);
+      while (r < q.r) r++, add(ds, v[r]);
+      while (l < q.l) remove(ds, v[l]), l++;
+      while (r > q.r) remove(ds, v[r]), r--;
       ans[q.i] = answer(ds, q);
     }
   }
