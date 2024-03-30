10894f3011dfdf3d2d953c6156e5a55b7ad2ec0f
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-03-29 21:26:38 -0300

    Create mulmod.cpp

---
+++ content/maths/mulmod.cpp
@@ -0,0 +1,8 @@
+/*
+  *Description:* Multiply two number fast, secure for number lesser than $2^57$
+  *Status:* Partially tested
+*/
+ll mulmod(ll a, ll b, ll m) {
+	ll r=a*b-(ll)((ld)a*b/m+.5)*m;
+	return r<0?r+m:r;
+}
