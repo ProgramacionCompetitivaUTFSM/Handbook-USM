0d440b8a062a443dfc41e1edeb70adec2ff6d17d
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-03-29 21:48:53 -0300

    Update pollard_rho.cpp

--- content/maths/pollard_rho.cpp
+++ content/maths/pollard_rho.cpp
@@ -4,14 +4,14 @@
 */
 ll pollard_rho(ll n) {
   if((n&1)^1) return 2;
-	ll x = 2, y=2, d=1;
-	ll c = rand()%n+1;
-	while(d==1){
-		x=(mulmod(x,x,n)+c)%n;
-		y=(mulmod(y,y,n)+c)%n;
-		y=(mulmod(y,y,n)+c)%n;
-		if(x>=y)d=__gcd(x-y,n);
-		else d=__gcd(y-x,n);
-	}
-	return d==n? pollard_rho(n):d;
+  ll x = 2, y=2, d=1;
+  ll c = rand()%n+1;
+  while(d==1) {
+    x=(mulmod(x,x,n)+c)%n;
+    y=(mulmod(y,y,n)+c)%n;
+    y=(mulmod(y,y,n)+c)%n;
+    if(x>=y)d=__gcd(x-y,n);
+    else d=__gcd(y-x,n);
+  }
+  return d==n? pollard_rho(n):d;
 }
