05f5b66d5a9ba7c681df1f6fd43c2e0a35800194
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-04-27 17:43:29 -0400

    Update segment-tree-lazy.cpp
    Better! thx: JOliva

--- content/data-structures/segment-tree-lazy.cpp
+++ content/data-structures/segment-tree-lazy.cpp
@@ -6,42 +6,51 @@ template<
   class T1, // answer value stored on nodes
   class T2, // lazy update value stored on nodes
   T1 merge(T1, T1), 
-  void pushUpd(T2&, T2&, int, int, int, int), // push update value from a node to another. parent -> child
-  void applyUpd(T2&, T1&, int, int)           // apply the update value of a node to its answer value. upd -> ans
+  void pushUpd(T2 /*parent*/, T2& /*child*/, int, int, int, int), // push update value from a node to another. parent -> child
+  void applyUpd(T2, T1&, int, int)           // apply the update value of a node to its answer value. upd -> ans
 >
 struct segment_tree_lazy{
   vector<T1> ST; vector<T2> lazy; vector<bool> upd;
   int n;
   void build(int i, int l, int r, vector<T1>&values){
-    if (l == r){
-        ST[i] = values[l];
-        return;
+    if (l == r) {
+      ST[i] = values[l];
+      return;
     }
     build(i << 1, l, (l + r) >> 1, values);
     build(i << 1 | 1, (l + r) / 2 + 1, r, values);
     ST[i] = merge(ST[i << 1], ST[i << 1 | 1]);
   }
   segment_tree_lazy(vector<T1>&values){
-    n = values.size(); ST.resize(n << 2 | 3);
-    lazy.resize(n << 2 | 3); upd.resize(n << 2 | 3, false);
+    n = values.size();
+    ST.resize(n << 2 | 3);
+    lazy.resize(n << 2 | 3);
+    upd.resize(n << 2 | 3, false);
     build(1, 0, n - 1, values);
   }
+  segment_tree_lazy(){}
   void push(int i, int l, int r){
     if (upd[i]){
       applyUpd(lazy[i], ST[i], l, r);
       if (l != r){
-        pushUpd(lazy[i], lazy[i << 1], l, r, l, (l + r) / 2);
-        pushUpd(lazy[i], lazy[i << 1 | 1], l, r, (l + r) / 2 + 1, r);
+        if (!upd[i << 1])
+          lazy[i << 1] = lazy[i];
+        else
+          pushUpd(lazy[i], lazy[i << 1], l, r, l, (l + r) / 2);
+        if (!upd[i << 1 | 1])
+          lazy[i << 1 | 1] = lazy[i];
+        else
+          pushUpd(lazy[i], lazy[i << 1 | 1], l, r, (l + r) / 2 + 1, r);
         upd[i << 1] = 1;
         upd[i << 1 | 1] = 1;
       }
       upd[i] = false;
-      lazy[i] = T2();
     }
   }
   void update(int i, int l, int r, int a, int b, T2 &u){
     if (l >= a and r <= b){
-      pushUpd(u, lazy[i], a, b, l, r);
+      if (!upd[i]) lazy[i] = u;
+      else pushUpd(u, lazy[i], a, b, l, r);
       upd[i] = true;
     }
     push(i, l, r);
@@ -55,7 +64,7 @@ struct segment_tree_lazy{
     if (a > b){
       update(0, b, u);
       update(a, n - 1, u);
-      return ;
+      return;
     }
     update(1, 0, n - 1, a, b, u);
   }
@@ -71,16 +80,9 @@ struct segment_tree_lazy{
     return merge(query(i << 1, l, mid, a, b), query(i << 1 | 1, mid + 1, r, a, b));
   }
   T1 query(int a, int b){
-    if (a > b) return merge(query(a, n - 1), query(0, b));
+    if (a > b){
+      return merge(query(a, n - 1), query(0, b));
+    }
     return query(1, 0, n - 1, a, b);
   }
 };
-ll merge(ll a, ll b){
-  return a + b;
-}
-void pushUpd(ll &u1, ll &u2, int l1, int r1, int l2, int r2){
-  u2 = u1;
-}
-void applyUpd(ll &u, ll &v, int l, int r){
-  v = (r - l + 1) * u;
-}
