19cf175a7bd4437d1599855494eebd78f9cd41b0
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-07-07 20:49:02 -0400

    Create line-container.cc

---
+++ content/data-structures/line-container.cc
@@ -0,0 +1,44 @@
+/*
+  Description: Line container, useful for convex hull trick
+*/
+struct Line {
+  mutable ll a, b, c;
+  bool operator < (Line r) const {
+    return a < r.a;
+  }
+  bool operator < (ll x) const {
+    return c < x;
+  }
+};
+// dynamically insert `a*x + b` lines and query for maximum
+// at any x all operations have complexity O(log N)
+struct LineContainer: multiset < Line, less < >> {
+  ll div(ll a, ll b) {
+    return a / b - ((a ^ b) < 0 && a % b);
+  }
+  bool isect(iterator x, iterator y) {
+    if (y == end()) return x -> c = INF, 0;
+    if (x -> a == y -> a) x -> c = x -> b > y -> b ? INF : -INF;
+    else x -> c = div(y -> b - x -> b, x -> a - y -> a);
+    return x -> c >= y -> c;
+  }
+
+  void add(ll a, ll b) {
+    // a *= -1, b *= -1 // for min
+    auto z = insert({
+      a,
+      b,
+      0
+    }), y = z++, x = y;
+    while (isect(y, z)) z = erase(z);
+    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+    while ((y = x) != begin() && (--x) -> c >= y -> c) isect(x, erase(y));
+  }
+
+  ll query(ll x) {
+    if (empty()) return -INF; // INF for min
+    auto l = * lower_bound(x);
+    return l.a * x + l.b;
+    // return -l.a * x - l.b; // for min
+  }
+};
