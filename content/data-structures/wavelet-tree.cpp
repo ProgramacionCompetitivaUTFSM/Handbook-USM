824c086941a5a12ff3114dcd16782baa6386d01e
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2024-07-20 18:55:54 -0400

    Update wavelet-tree.cpp

--- content/data-structures/wavelet-tree.cpp
+++ content/data-structures/wavelet-tree.cpp
@@ -2,88 +2,69 @@
  *Description:* Succint data structure to store strings in compressed space. It generalizes rank and select operations. Runs on $O(log sigma)$, where $sigma$ is the size of the alphabet
  *Status:* Tested
 */
-struct WT {
-  typedef vi::iterator iter;
-  vvi r0; vi arrCopy; int n, s, q, w;
-  void build(iter b, iter e, int l, int r, int u) {
-    if (l == r) return;
-    int m = (l + r) / 2;
-    r0[u].reserve(e - b + 1); r0[u].pb(0);
-    for (iter it = b; it != e; ++it)
-      r0[u].pb(r0[u].back() + (*it <= m));
-    iter p = stable_partition(b, e, [=](int i) { return i <= m; });
-    build(b, p, l, m, u * 2); build(p, e, m + 1, r, u * 2 + 1);
-  }
-  int range(int a, int b, int l, int r, int u) {
-    if (r < q or w < l) return 0;
-    if (q <= l && r <= w) return b - a;
-    int m = (l + r) / 2, za = r0[u][a], zb = r0[u][b];
-    return range(za, zb, l, m, u * 2) + range(a - za, b - zb, m + 1, r, u * 2 + 1);
-  }
-  WT(vi arr, int sigma) { // arr[i] in [0,sigma)
-      n = sz(arr); s = sigma; r0.resize(s * 2);
-      arrCopy = arr;
-      build(all(arr), 0, s - 1, 1);
-  }
-  // k in [1,n], [a,b) is 0-indexed, -1 if error
-  int quantile(int k, int a, int b) {
-    if (/*a < 0 or b > n or*/ k < 1 or k > b - a) return -1;
-    int l = 0, r = s - 1, u = 1, m, za, zb;
-    while (l != r) {
-      m = (l + r) / 2;
-      za = r0[u][a], zb = r0[u][b], u *= 2;
-      if (k <= zb - za) a = za, b = zb, r = m;
-      else k -= zb - za, a -= za, b -= zb, l = m + 1, ++u;
+template<typename T, int MAXLOG>
+struct wavelet_tree {
+  struct node {
+    succinct_indexable_dictionary sid;
+    node *ch[2];
+    node() = default;
+    node(size_t len) : sid(len + 1), ch{nullptr} {}
+  };
+  node *root;
+  node *build(vector<T> &v, vector<T> &rbuff, int bit, int l, int r) {
+    if (l >= r or bit == -1) return nullptr;
+    node *_node = new node(r-l);
+    ll left = 0, right = 0; 
+    for (ll k = l; k < r; k++) {
+      if ((v[k] >> bit) & 1)
+        rbuff[right++] = v[k], _node->sid.set(k - l);
+      else v[l + left++] = v[k];
     }
-    return r;
+    for (int k = 0; k < right; k++) v[l + left + k] = rbuff[k];
+    _node->sid.build();
+    _node->ch[0] = build(v, rbuff, bit - 1, l, l + left);
+    _node->ch[1] = build(v, rbuff, bit - 1, l + left, r);
+    return _node;
   }
-  // counts numbers in [x,y] in positions [a,b)
-  int range(int x, int y, int a, int b) {
-    if (y < x or b <= a) return 0;
-    q = x, w = y;
-    return range(a, b, 0, s - 1, 1);
+  wavelet_tree() = default;
+  wavelet_tree(vector<T> &v) {
+    vector<T> rbuff(v.size());
+    root = build(v, rbuff, MAXLOG - 1, 0, v.size());
   }
-  // count occurrences of x in positions [0,k)
-  int rank(int x, int k) {
-    int l = 0, r = s - 1, u = 1, m, z;
-    while (l != r) {
-      m = (l + r) / 2;
-      z = r0[u][k], u *= 2;
-      if (x <= m) k = z, r = m;
-      else k -= z, l = m + 1, ++u;
-    }
-    return k;
+  int rank(node *t, int l, int r, const T&x, int level) {
+    if (l >= r or t == nullptr) return 0;
+    if (level == -1) return r-l;
+    bool f = (x >> level) & 1;
+    l = t->sid.rank(f, l), r = t->sid.rank(f, r);
+    return rank(t->ch[f], l, r, x, level-1);
   }
-  void pb(int x) { // x in [0,sigma)
-    int l = 0, r = s - 1, u = 1, m, p; ++n;
-    while (l != r) {
-      m = (l + r) / 2;
-      p = (x <= m);
-      r0[u].pb(r0[u].back() + p);
-      u *= 2;
-      if (p) r = m;
-      else l = m + 1, ++u;
-    }
+  int rank(const T &x, int r) { return rank(root, 0, r, x, MAXLOG-1); }
+  T kth(node *t, int l, int r, int k, int level) {
+    if (l >= r || t == nullptr) return 0;
+    int cnt = t->sid.rank(false, r) - t->sid.rank(false, l);
+    bool f = cnt <= k;
+    l = t->sid.rank(f, l), r = t->sid.rank(f, r);
+    if (f) return kth(t->ch[f], l, r, k - cnt, level - 1) | (T(1) << level);
+    return kth(t->ch[f], l, r, k, level - 1);
   }
-  void pop_back() { // doesn't check if empty
-    int l = 0, r = s - 1, u = 1, m, p, k; --n;
-    while (l != r) {
-      m = (l + r) / 2;
-      k = sz(r0[u]), p = r0[u][k - 1] - r0[u][k - 2];
-      r0[u].pop_back(); u *= 2;
-      if (p) r = m;
-      else l = m + 1, ++u;
-    }
+  // k-th(0-indexed) smallest number in v[l,r]
+  T kth_smallest(int l, int r, int k) {
+    return kth(root, l, r+1, k, MAXLOG - 1);
   }
-  void swap_adj(int i) { // swap arr[i] with arr[i+1], i in [0,n-1)
-    int &x = arrCopy[i], &y = arrCopy[i + 1];
-    int l = 0, r = s - 1, u = 1;
-    while (l != r) {
-      int m = (l + r) / 2, p = (x <= m), q = (y <= m);
-      if (p != q) { r0[u][i + 1] ^= r0[u][i] ^ r0[u][i + 2]; break; }
-      u *= 2; if (p) r = m;
-      else l = m + 1, ++u;
-    }
-    swap(x, y);
+  // k-th(0-indexed) largest number in v[l,r]
+  T kth_largest(int l, int r, int k) {
+    return kth(l, r, r - l - k);
+  }
+  int range_freq(node *t, int l, int r, T upper, int level) {
+    if (t == nullptr || l >= r) return 0;
+    bool f = ((upper >> level) & 1);
+    int ret = 0;
+    if (f) ret += t->sid.rank(false, r) - t->sid.rank(false, l);
+    l = t->sid.rank(f, l), r = t->sid.rank(f, r);
+    return range_freq(t->ch[f], l, r, upper, level - 1) + ret;
+  }
+  // count i s.t. (l <= i < r) && (v[i] < upper)
+  int range_freq(int l, int r, T upper) {
+    return range_freq(root, l, r, upper, MAXLOG - 1);
   }
 };
