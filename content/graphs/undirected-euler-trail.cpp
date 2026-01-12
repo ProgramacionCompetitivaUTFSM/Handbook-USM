2c04e5990f523c0f495ec9b01ce5ad625bf3ecf4
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2026-01-12 18:13:49 -0300

    Add undirected Eulerian trail implementation
    Implements an undirected Eulerian trail using Hierholzer's algorithm, handling self-loops and parallel edges. Provides methods to add edges and solve for the trail.

---
+++ content/graphs/undirected-euler-trail.cpp
@@ -0,0 +1,76 @@
+/**
+ * Eulerian Trail (Undirected Graphs)
+ * 
+ * Finds an Eulerian trail/circuit in an undirected graph using Hierholzer's algorithm.
+ * Handles self-loops and parallel edges.
+ * 
+ * Usage:
+ *   undirected_euler_trail euler(n);
+ *   euler.add_edge(u, v);
+ *   if (euler.solve()) {
+ *       euler.vertices;  // vertex sequence (size m+1)
+ *       euler.edges;     // edge index sequence (size m)
+ *   }
+ * 
+ * Complexity: O(V + E)
+ * Tested: https://judge.yosupo.jp/problem/eulerian_trail_undirected
+ */
+struct undirected_euler_trail {
+  ll n, m = 0;
+  vector<vector<ll>> adj;
+  vector<pair<ll, ll>> E;
+  vector<ll> deg, vertices, edges;
+
+  undirected_euler_trail(ll n) : n(n), adj(n), deg(n) {}
+
+  void add_edge(ll u, ll v) {
+    adj[u].push_back(m);
+    if (u != v) adj[v].push_back(m);
+    E.push_back({u, v});
+    deg[u]++, deg[v]++, m++;
+  }
+
+  ll find_start() {
+    ll s = -1, codd = 0;
+    for (ll v = 0; v < n; v++) {
+      if (deg[v]&1) codd++, s = v;
+      else if (deg[v] > 0 && s < 0)
+        s = v;
+    }
+    return (codd == 0 || codd == 2) ? s : -2;
+  }
+
+  bool solve() {
+    if (m == 0) { vertices = {0}; return true; }
+
+    ll s = find_start();
+    if (s == -2) return false;
+
+    vector<ll> ep(n);
+    edges.resize(m);
+    vector<bool> alive(m, true);
+    ll pp = 0, pq = m, v = s;
+
+    while (pp < pq) {
+      if (ep[v] >= adj[v].size()) {
+        if (pp == 0)
+          return false;
+        edges[--pq] = edges[--pp];
+        v ^= E[edges[pq]].first ^ E[edges[pq]].second;
+      } else {
+        ll e = adj[v][ep[v]++];
+        if (!alive[e])
+          continue;
+        alive[e] = false;
+        edges[pp++] = e;
+        v ^= E[e].first ^ E[e].second;
+      }
+    }
+    vertices = {s};
+    for (ll e : edges) {
+      s ^= E[e].first ^ E[e].second;
+      vertices.push_back(s);
+    }
+    return true;
+  }
+};
