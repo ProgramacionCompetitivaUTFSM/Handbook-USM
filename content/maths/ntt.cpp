cca4049e6a2dbab003a2fff9f4365a270f93d1d5
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2026-01-23 09:44:05 -0300

    Fix overflow: use ll instead of int

--- content/maths/ntt.cpp
+++ content/maths/ntt.cpp
@@ -7,32 +7,32 @@
 */
 template<int mod, int root>
 struct NTT {
-  void ntt(int* x, int* temp, int* roots, int N, int skip) {
+  void ntt(ll* x, ll* temp, ll* roots, int N, int skip) {
     if (N == 1) return;
     int n2 = N/2;
     ntt(x, temp, roots, n2, skip*2);
     ntt(x+skip, temp, roots, n2, skip*2);
     for (int i = 0; i < N; i++) temp[i] = x[i*skip];
     for (int i = 0; i < n2; i++) {
-      int s = temp[2*i], t = temp[2*i+1] * roots[skip*i];
+      ll s = temp[2*i], t = temp[2*i+1] * roots[skip*i] % mod;
       x[skip*i] = (s + t) % mod;
-      x[skip*(i+n2)] = (s - t) % mod;
+      x[skip*(i+n2)] = ((s - t) % mod + mod) % mod;
     }
   }
-  void ntt(vector<int>& x, bool inv = false) {
-    int e = binpow(root, (mod-1)/(x.size()), mod);
+  void ntt(vector<ll>& x, bool inv = false) {
+    ll e = binpow(root, (mod-1)/((int)x.size()), mod);
     if (inv) e = binpow(e, mod-2, mod);
-    vector<int> roots(x.size(), 1), temp = roots;
-    for (int i = 1; i < x.size(); i++) roots[i] = roots[i-1] * e % mod;
+    vector<ll> roots(x.size(), 1), temp = roots;
+    for (int i = 1; i < (int)x.size(); i++) roots[i] = roots[i-1] * e % mod;
     ntt(&x[0], &temp[0], &roots[0], x.size(), 1);
   }
-  vector<int> conv(vector<int> a, vector<int> b) {
+  vector<ll> conv(vector<ll> a, vector<ll> b) {
     int s = a.size()+b.size()-1;
     if (s <= 0) return {};
     int L = s > 1 ? 32 - __builtin_clz(s - 1) : 0, n = 1 << L;
     a.resize(n); ntt(a);
     b.resize(n); ntt(b);
-    vector<int> c(n); int d = binpow(n, mod-2, mod);
+    vector<ll> c(n); ll d = binpow(n, mod-2, mod);
     for (int i = 0; i < n; i++) c[i] = a[i] * b[i] % mod * d % mod;
     ntt(c, true); c.resize(s);
     for (int i = 0; i < n; i++) if(c[i] < 0) c[i] += mod;
