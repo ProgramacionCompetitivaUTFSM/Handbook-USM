84e237166aa2c5a0df4d5cf17efc739260795083
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2026-01-23 09:43:35 -0300

    Fix bug: simplify merge to avoid index -1

--- content/maths/divisors.cpp
+++ content/maths/divisors.cpp
@@ -4,22 +4,14 @@
   *Status:* Highly tested
 */
 vector<ll> get_divisors(ll n) {
-  vector<ll> left, right, ans;
+  vector<ll> left, right;
   for (ll i = 1; i * i <= n; i++)
     if (n % i == 0) {
+      left.push_back(i);
       if (i != n / i)
         right.push_back(n / i);
-      left.push_back(i);
     }
-  ans.resize(left.size() + right.size());
   reverse(begin(right), end(right));
-  ll i = 0, j = 0;
-  while (i < left.size() and j < right.size()) {
-    if (left[i] < right[j]) 
-      ans[i + j - 1] = left[i++];
-    else ans[i + j - 1] = right[j++];
-  }
-  while(i < left.size()) ans[i + j - 1] = left[i++];    
-  while(j < right.size()) ans[i + j - 1] = right[j++];
-  return ans;
+  for (ll x : right) left.push_back(x);
+  return left;
 }
