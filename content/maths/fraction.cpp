32502d9bfcf5e2957c2d5ccbc5918dac0dad0df3
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2026-01-23 09:43:50 -0300

    Fix overflow: cast to ll in comparisons, fix add function

--- content/maths/fraction.cpp
+++ content/maths/fraction.cpp
@@ -2,25 +2,23 @@
   *Description:* Just a fraction, every operation runs on $O(log(min(p, q)))$
   *Status:* Not tested
 */
-#define lcm(a, b) ( gcd(a, b) ? ( (a)*(b) ) / gcd(a, b) ): 0
-template <typename T>
+template <typename T = ll>
 struct Fraction {
   T p, q;
   Fraction() {}
   Fraction(T p, T q): p(p), q(q) {
     if (q < 0) this->p = -p, this->q = -q;
   }
-  bool operator<(const Fraction o) {
-    return p*o.q < o.p*q;
+  bool operator<(const Fraction o) const {
+    return (ll)p*o.q < (ll)o.p*q;
   }
   Fraction simplify(Fraction f){
-    ll g = __gcd(f.p, f.q);
+    T g = __gcd(f.p, f.q);
     return Fraction(f.p/g, f.q/g);
   }
   Fraction add(Fraction f){
-    ll l = lcm(q, f.q);
-    p *= (l/q);
-    p += f.p * (l/f.q);
-    return simplify(Fraction(p, l));
+    T l = q / __gcd(q, f.q) * f.q;
+    T np = p * (l/q) + f.p * (l/f.q);
+    return simplify(Fraction(np, l));
   }
 };
