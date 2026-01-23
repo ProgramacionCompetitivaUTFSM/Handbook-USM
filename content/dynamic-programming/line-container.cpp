af7405838a26db752c865a96400806d5c6c66afb
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2026-01-23 09:42:45 -0300

    Fix overflow: use ll instead of int, add INF constant

--- content/dynamic-programming/line-container.cpp
+++ content/dynamic-programming/line-container.cpp
@@ -2,13 +2,14 @@
  *Description:* Dynamically insert a*x + b lines and query for maximum at any x and operations have complexity O(log N)
  *Status:* Tested on CSES
 */
+const ll INF = 1e15;
 struct Line {
-    mutable int a, b, c;
+    mutable ll a, b, c;
     bool operator<(Line r) const { return a < r.a; }
-    bool operator<(int x) const { return c < x; }
+    bool operator<(ll x) const { return c < x; }
 };
 struct LineContainer : multiset<Line, less<>> {
-    int div(int a, int b) {
+    ll div(ll a, ll b) {
         return a / b - ((a ^ b) < 0 && a % b);
     }
     bool isect(iterator x, iterator y) {
@@ -17,14 +18,14 @@ struct LineContainer : multiset<Line, less<>> {
         else x->c = div(y->b - x->b, x->a - y->a);
         return x->c >= y->c;
     }
-    void add(int a, int b) {
+    void add(ll a, ll b) {
         // a *= -1, b *= -1 // for min
         auto z = insert({a, b, 0}), y = z++, x = y;
         while (isect(y, z)) z = erase(z);
         if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
         while ((y = x) != begin() && (--x)->c >= y->c) isect(x, erase(y));
     }
-    int query(int x) {
+    ll query(ll x) {
         if (empty()) return -INF; // INF for min
         auto l = *lower_bound(x);
         return l.a * x + l.b;
