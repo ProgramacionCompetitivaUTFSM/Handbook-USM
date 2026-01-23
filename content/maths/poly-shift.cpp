aaaf36c5390de9d56d8bf5e1de887b5cb3558aa6
Sebastián Torrealba <sebastian.torrealba@usm.cl>
2026-01-23 09:44:08 -0300

    Fix overflow: use ll instead of int

--- content/maths/poly-shift.cpp
+++ content/maths/poly-shift.cpp
@@ -2,20 +2,20 @@
  *Description:* Solves $f(x + c) = sum_0^(n-1) b_i dot x^i$
  *Status:* Tested
 */
-vector<int> polyShift(vector<int> &a, int shift) {
+vector<ll> polyShift(vector<ll> &a, ll shift) {
   // change for any mod for ntt
-  const int mod = 998244353;
+  const ll mod = 998244353;
   NTT<998244353, 3> ntt;
   int n = a.size() - 1;
   Factorial f(n, mod);
-  vector<int> x(n+1), y(n+1);
-  int cur = 1;
+  vector<ll> x(n+1), y(n+1);
+  ll cur = 1;
   for (int i = 0; i <= n; i++) {
     x[i] = cur * f.finv[i] % mod;
-    cur = (cur * shift) % mod;
+    cur = cur * shift % mod;
     y[i] = a[n - i] * f.f[n-i] % mod;
   }
-  vector<int> tmp = ntt.conv(x, y), res(n+1);
+  vector<ll> tmp = ntt.conv(x, y), res(n+1);
   for (int i = 0; i <= n; i++)
     res[i] = tmp[n-i] * f.finv[i] % mod;
   return res;
