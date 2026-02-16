ef55a7d6860746e0b33742c645770317b39430ce
Sebastián Torrealba <83615350+storrealbac@users.noreply.github.com>
2026-02-16 13:30:59 -0300

    Fix type casting in segment distance calculation (#72)

--- content/geometry/primitives/segment-distance.cpp
+++ content/geometry/primitives/segment-distance.cpp
@@ -6,6 +6,6 @@ using P = point2d<ld>;
 ld seg_dist(P s, P e, P p) {
   if (s.sqdist(e) < 1e-18) return (p-s).abs();
   auto d = (e-s).norm();
-  auto t = min(d, max(0.0, (p-s)|(e-s)));
+  auto t = min(d, max((ld)0.0, (p-s)|(e-s)));
   return ((p-s)*d - (e-s)*t).abs() / d;
 }
